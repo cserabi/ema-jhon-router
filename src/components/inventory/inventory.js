af9d007ab06cffed1987e6252a7080467ccf0d13
cserabi <rabiulislamhstucse@gmail.com>
2021-10-01 15:31:07 +0600

    update

--- src/components/inventory/inventory.js
+++ src/components/inventory/inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const inventory = () => {
   return (
     <div>
-      <h2>This is inventory</h2>
+      <h2>This is inventory design by rabiul islam</h2>
     </div>
   );
 };
